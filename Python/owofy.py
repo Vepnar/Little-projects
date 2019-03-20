5030899454c2677c9025d4a0f5b5685f9475c5fa
Arjan <26712719+Vepnar@users.noreply.github.com>
2019-03-20 14:14:21 +0100

    Added owofy + remove license

---
+++ Python/owofy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import random
+
+# Use at your own risk
+
+faces = ['(・`ω´・)', ';;w;;', 'owo', 'OwO','>.<', 'uwu', '( ˘͈ ᵕ ˘͈♡)','UwU', '>w<', '^w^','>.>', '(つ✧ω✧)つ', '(/ =ω=)/']
+
+def owofy(msg):
+    face =' '+faces[random.randint(0,len(faces)-1)]
+    msg = re.sub(r'(?:l|r)', 'w',msg)
+    msg = re.sub(r'(?:L|R)', 'W',msg)
+    msg = re.sub(r'n([aeiou])','ny$1',msg)
+    msg = re.sub(r'N([aeiou])','ny$1',msg)
+    msg = re.sub(r'N([AEIOU])','ny$1',msg)
+    msg = re.sub(r'ove','uv',msg)
+
+    return re.sub(r'!+',face,msg)
+    
+if __name__ == '__main__':
+    import sys
+    args = ' '.join(sys.argv[1:])
+
+print(owofy(args))
