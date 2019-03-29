9689ddcdd46156d2e93d432ac430f69bf7141a0e
Arjan <26712719+Vepnar@users.noreply.github.com>
2019-03-29 16:46:56 +0100

    I should not add this but I did anyway

---
+++ Cpp/aaaa.cpp
@@ -0,0 +1,32 @@
+/*
+    Author: Arjan de Haan (Vepnar)
+    Please ignore this file
+
+*/
+
+
+#include <iostream>
+
+#define a int
+#define aa {
+#define aaa }
+#define aaaa 0
+#define aaaaaa <<
+#define aaaaaaa "Hello, World!\n"
+#define aaaaaaaa cout
+#define aaaaaaaaa return
+#define aaaaaaaaaa ;
+#define aaaaaaaaaaa main
+#define aaaaaaaaaaaa (
+#define aaaaaaaaaaaaa )
+#define aaaaaaaaaaaaaa for
+#define aaaaaaaaaaaaaaa ::
+#define aaaaaaaaaaaaaaaa std
+
+a aaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa
+aa
+    aaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaa aaaaaaaaaa aaaaaaaaaaaaa aa
+    aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaa aaaaaa aaaaaaa aaaaaaaaaa
+    aaa
+    aaaaaaaaa aaaa aaaaaaaaaa
+aaa
