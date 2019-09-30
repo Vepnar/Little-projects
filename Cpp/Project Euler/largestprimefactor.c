1167540dfdf347b08260fe16ef15b8dc6d5a807a
Vepnar <26712719+Vepnar@users.noreply.github.com>
2019-09-30 10:18:43 +0200

    Problem 3 Euler

--- Cpp/Project Euler/largestprimefactor.c
+++ Cpp/Project Euler/largestprimefactor.c
@@ -1,11 +1,33 @@
 // Author: Arjan de Haan (Vepnar)
 // https://en.wikipedia.org/wiki/Fibonacci_number
-// https://projecteuler.net/problem=2
+// https://projecteuler.net/problem=3
 
 #include <stdio.h>
 
 int main(void)
 {
-    //WIP
-
+    long long n = 600851475143;
+    int i =2;
+    int counter=0;
+    int factors[10];
+    while(i * i <= n)
+    {
+        if(n % i != 0)
+        {
+            i++;
+        }
+        else
+        {
+            n = n / i;
+            factors[counter] = i;
+            counter++;
+        }
+    }
+    if(n > 1)
+    { 
+        factors[counter] = n;
+        counter++;
+    }
+    for(int i=0;i<counter;i++)
+        printf("%d\n",factors[i]);
 }
